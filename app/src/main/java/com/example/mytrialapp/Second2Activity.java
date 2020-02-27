0ad7f4763c3335b5b2adccb6386be5929e87470d
solly <214293939@mycput.ac.za>
2020-02-27 16:16:49 +0200

    Structure of app cycle life...

---
+++ app/src/main/java/com/example/mytrialapp/Second2Activity.java
@@ -0,0 +1,14 @@
+package com.example.mytrialapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Second2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second2);
+    }
+}
